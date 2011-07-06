71624a023a080533f42f9f2455f9110fcc3bdda5
Michael Fenimore <michael.fenimore@cbs.com>
2011-07-06 17:39:05 -0400

    Array lesson created

---
+++ Array/Classes/ArrayAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  ArrayAppDelegate.h
+//  Array
+//
+//  Created by Fenimore, Michael on 7/6/11.
+//  Copyright 2011 --MichaelFenimore--. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArrayAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
