ba4ff33d7c489a17f64473f6607a26e18d702a7e
Michael Fenimore <michael.fenimore@cbs.com>
2011-07-27 22:34:10 -0400

    Build Files

---
+++ Week-07-Homework/Pong/Classes/Classes-1.moved-aside/SpriteHelpers.h
@@ -0,0 +1,16 @@
+//
+//  SpriteHelpers.h
+//  Pong
+//
+//  Created by Michael Fenimore on 7/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SpriteHelpers : UIView {
+
+}
+
+@end
---
+++ Week-07-Homework/Pong/Classes/Classes-1.moved-aside/SpriteHelpers.m
@@ -0,0 +1,37 @@
+//
+//  SpriteHelpers.m
+//  Pong
+//
+//  Created by Michael Fenimore on 7/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SpriteHelpers.h"
+
+
+@implementation SpriteHelpers
+
+
+- (id)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code.
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code.
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
